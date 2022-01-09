758c69992e3db699fe40416a1aafb0cfcee4d1ce
Uptight <RobloxArchiver@protonmail.com>
2022-01-09 03:21:39 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 _G.AssetDownloader = {}
 
-function DownloadAsset(fileName, URL)
+function _G.AssetDownloader.DownloadAsset(fileName, URL)
     if isfile(fileName) then
         if readfile(fileName) ~= game:HttpGet(URL) then
            writefile(fileName, game:HttpGet(URL);
