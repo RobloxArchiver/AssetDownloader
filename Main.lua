eceefc4c0fa89fb152b6c19afe531affb0e57ca4
Uptight <uptight283@gmail.com>
2022-02-11 21:00:14 -0500

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,17 +1,12 @@
 AssetDownloader = {}
 
+local getasset = getsynasset or getcustomasset -- I didn't know Script-Ware could use gca
+
 function DownloadAsset(fileName, URL)
     if not isfile(fileName) then
         writefile(fileName, game:HttpGet(URL))
     end
 end
-
---[[
-    beggining work on the *FOLDER* downloader
-    These are two seperate functions
-    The FOLDER DOWNLOADER MAKE THE FOLDER ASKED FOR AND DOWNLOADS THE ASSETS TO IT
-    DOWNLOADASSET DOWNLOADS JUST THE FILE TO WORKSPACE!
-]]
 
 function AssetFolder(folderName)
     if not isfolder(folderName) then
@@ -19,11 +14,4 @@ function AssetFolder(folderName)
     end
 end
 
---[[
-
-    I made it simpler than it was, it will CREATE the folder.
-
-    Usage example:
-    (Examples folder, FolderAsset)
-
-]]
+return AssetDownloader
