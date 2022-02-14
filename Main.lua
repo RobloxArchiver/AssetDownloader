0a2ef1a91de6fd3b30051bd0f952caf607c0c7da
Zer0D_ay <95448029+RobloxArchiver@users.noreply.github.com>
2022-02-14 05:37:10 -0500

    syn.request

--- Main.lua
+++ Main.lua
@@ -1,10 +1,12 @@
 AssetDownloader = {}
 
 local getasset = getsynasset or getcustomasset -- I didn't know Script-Ware could use gca
+local net =  syn.request or httpget or http_request or game:HttpGetAsync or game:GetHttp -- Overcomplicating but apparently use GetHttp last!
+-- if you know anymore I can use please make a pull request :)
 
 function DownloadAsset(fileName, URL)
     if not isfile(fileName) then
-        writefile(fileName, game:HttpGet(URL))
+        writefile(fileName, net(URL))
     end
 end
 
