826f7df11e0ab36399926af496fe31ebb66fd49e
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-08 10:41:27 -0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,28 +1,18 @@
 AssetDownloader = {}
 
-local getasset = getsynasset or getcustomasset -- I didn't know Script-Ware could use gca
-local net =  syn.request or httpget or http_request or game:HttpGetAsync or requests
--- if you know anymore I can use please make a pull request :)
+local getasset = getsynasset or getcustomasset
+local net = game:HttpGetAsync or http_request or requests or syn.request
 
 function DownloadAsset(fileName, URL)
     if not isfile(fileName) then
-        writefile(fileName, net(URL))
-    end
-end
+        writefile(fileName, net(URL));
+    end;
+end;
 
 function AssetFolder(folderName)
     if not isfolder(folderName) then
-        makefolder(folderName)
-    end
-end
+        makefolder(folderName);
+    end;
+end;
 
 return AssetDownloader
-
---[[
-
-Supported List:
-    KRNL (Free)
-    Script-Ware ($20)
-    Synapse X (x.synapse.to Synapse winning $20)
-
-]]
