834b277d303ab738dea954c8531ca07730efb32f
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-12 21:39:45 -0400

    Added `UseAsset()`

--- Main.lua
+++ Main.lua
@@ -1,18 +1,22 @@
-AssetDownloader = {}
+local AssetDownloader = {}
 
 local getasset = getsynasset or getcustomasset
 local net = game:HttpGetAsync or http_request or requests or syn.request
 
-function DownloadAsset(fileName, URL)
+function AssetDownloader.DownloadAsset(fileName, URL)
     if not isfile(fileName) then
         writefile(fileName, net(URL));
     end;
 end;
 
-function AssetFolder(folderName)
+function AssetDownloader.AssetFolder(folderName)
     if not isfolder(folderName) then
         makefolder(folderName);
     end;
 end;
 
-return AssetDownloader
+function AssetDownloader.UseAsset(asset)
+    return getasset(asset);
+end;
+
+return AssetDownloader;
