af5beff9c316c33c9c9cf398bcab29ee4f87fce5
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-13 00:04:46 -0400

    Delete Download2Assets.lua

--- Examples/Download2Assets.lua
+++
@@ -1,8 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/AssetDownloader/main/Main.lua"))()
-DownloadAsset("Checkmark.png", "https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/Assets/testing.png")
-DownloadAsset("MOOSH.png", "https://www.pinclipart.com/picdir/middle/447-4473590_random-png-transparent-background-imagenes-random-png-clipart.png")
-
-local Checkmark = getasset("Checkmark.png")
-local MOOSH = getasset("MOOSH.png")
-
-print("Image 1: " .. Checkmark .. "CHECKMARK \n Image 2: " .. MOOSH .. "MOOSHROOM")
