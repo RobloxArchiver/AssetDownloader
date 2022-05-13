6e6061c66d1e9e58ee48027888915e9446ea6bb5
Archiver <95448029+RobloxArchiver@users.noreply.github.com>
2022-05-13 00:04:51 -0400

    Delete FolderAsset.lua

--- Examples/FolderAsset.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RobloxArchiver/AssetDownloader/main/Main.lua"))()
-AssetFolder("Assets")
-DownloadAsset("Assets/Checkmark.png", "https://raw.githubusercontent.com/RobloxArchiver/Xapper-Addon-Loader/main/Assets/testing.png")
-
-local check = getasset("Assets/Checkmark.png")
-
-print(check)
